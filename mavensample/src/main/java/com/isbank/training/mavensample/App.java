98cc9a0abff93b4655bdb847b6f2e58e6a71248b
Onur Vuranok <onurvuranok@gmail.com>
2016-09-24 17:48:03 +0300

    executable jar with main class

---
+++ mavensample/src/main/java/com/isbank/training/mavensample/App.java
@@ -0,0 +1,13 @@
+package com.isbank.training.mavensample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
