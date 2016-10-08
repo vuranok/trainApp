186811efc41b5b0819242fde1b41cf61a5abf8b7
Onur Vuranok <onurvuranok@gmail.com>
2016-10-08 17:17:19 +0300

    Externalized message injected to constructor by the help of namespace c

--- springhelloworld/src/main/java/com/isbank/spring/consinject/ConfigurableMessageProvider.java
+++ springhelloworld/src/main/java/com/isbank/spring/consinject/ConfigurableMessageProvider.java
@@ -1,7 +1,7 @@
 package com.isbank.spring.consinject;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.isbank.spring.message.MessageProvider;
@@ -12,7 +12,8 @@ public class ConfigurableMessageProvider implements MessageProvider {
 	private String message;
 	
 	@Autowired
-	public ConfigurableMessageProvider(@Value("Configurable Message with the help of Value annotation and Autowired annotation") String message) {
+//	public ConfigurableMessageProvider(@Value("Configurable Message with the help of Value annotation and Autowired annotation") String message) {
+	public ConfigurableMessageProvider(String message) {
 
 		this.message = message;
 	}
