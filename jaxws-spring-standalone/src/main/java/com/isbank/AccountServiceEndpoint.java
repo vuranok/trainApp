e89c7ff351d453c00c01743a21488ba14f713836
IS96160 <onur.vuranok@softtech.isbank.com.tr>
2016-10-23 18:01:07 +0300

    ADDED: component-scan, annotations and endpoint definitions are added

--- jaxws-spring-standalone/src/main/java/com/isbank/AccountServiceEndpoint.java
+++ jaxws-spring-standalone/src/main/java/com/isbank/AccountServiceEndpoint.java
@@ -6,6 +6,14 @@ import javax.jws.WebService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/*
+ * JAX–WS requires working with dedicated endpoint classes.
+ * The endpoint instances are defined and managed as Spring beans themselves; 
+ * they will be registered with the JAX–WS engine but their lifecycle will be up to the Spring application context. 
+ * This means that Spring functionality like explicit dependency injection may be applied to the endpoint instances. 
+ * Of course, annotation-driven injection through @Autowired will work as well.	
+ */
+
 @Service("accountServiceEndpoint")
 @WebService(serviceName="AccountService")
 public class AccountServiceEndpoint {
--- jaxws-spring-standalone/src/main/java/com/isbank/AccountServiceImpl.java
+++ jaxws-spring-standalone/src/main/java/com/isbank/AccountServiceImpl.java
@@ -5,6 +5,26 @@ import java.util.TreeMap;
 
 import org.springframework.stereotype.Service;
 
+/*
+ * @Service : annotation to specify that the bean provides services that other beans may require, 
+ * passing in the bean name as the parameter
+ * 
+ * @Component has the same effect as @Service Both annotations are instructing Spring that the annotated class is a
+	candidate for autodetection using annotation-based configuration and classpath scanning. However, since the
+	InjectSimpleConfig class is storing the application configuration, rather than providing a business service, using
+	@Component makes more sense. Practically, @Service is a specialization of @Component, which indicates that the
+	annotated class is providing a business service to other layers within the application.
+	
+	@Component("injectSimpleConfig")
+		public class InjectSimpleConfig {
+		private String name = "John Smith";
+		private int age = 35;
+		private float height = 1.78f;
+		private boolean programmer = true;
+		private Long ageInSeconds = 1103760000L;
+	}
+
+ */
 @Service
 public class AccountServiceImpl implements AccountService {
 
